1645c03c00570751eddbc2387b3d4cb16ace48f7
muchomuchacho <4213495+muchomuchacho@users.noreply.github.com>
2020-06-23 15:10:29 +0100

    Change how we take samples from OUNoise

--- utils.py
+++ utils.py
@@ -16,6 +16,7 @@ class OUNoise:
         self.mu = mu * np.ones(size)
         self.theta = theta
         self.sigma = sigma
+        self.size = size
         self.seed = random.seed(seed)
         self.reset()
 
@@ -26,8 +27,9 @@ class OUNoise:
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array(
-                [random.random() for i in range(len(x))])
+        dx = self.theta * (
+                self.mu - x) + self.sigma * np.random.standard_normal(
+                        self.size)
         self.state = x + dx
         return self.state
 
